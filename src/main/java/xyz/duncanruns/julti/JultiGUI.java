40e037216331831e57c8a4de221da8dd455434c2
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2022-10-11 20:48:34 -0300

    Update JultiGUI.java

--- src/main/java/xyz/duncanruns/julti/JultiGUI.java
+++ src/main/java/xyz/duncanruns/julti/JultiGUI.java
@@ -1,27 +1,30 @@
 package xyz.duncanruns.julti;
 
-import xyz.duncanruns.julti.util.ResourceUtil;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.io.IOException;
 
 public class JultiGUI extends JFrame implements WindowListener {
     private final Julti julti;
+    private final JPanel panel;
 
     public JultiGUI(Julti julti) {
         this.julti = julti;
+        panel = new JPanel();
         setupWindow();
         addWindowListener(this);
     }
 
     private void setupWindow() {
+        panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        panel.setLayout(new GridLayout(2, 2));
+
+        add(panel, BorderLayout.CENTER);
         setTitle("Julti");
+        pack();
         setVisible(true);
         setResizable(false);
-        getContentPane().add(new Button());
     }
 
     @Override
