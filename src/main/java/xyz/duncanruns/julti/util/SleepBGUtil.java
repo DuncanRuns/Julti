4ce050e931451b202f2b9f24728dafa3124bfc15
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2022-12-04 04:53:45 -0400

    Create SleepBGUtil.java

---
+++ src/main/java/xyz/duncanruns/julti/util/SleepBGUtil.java
@@ -0,0 +1,28 @@
+package xyz.duncanruns.julti.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class SleepBGUtil {
+    private static final Path SLEEP_LOCK_PATH = Paths.get(System.getProperty("user.home")).resolve("sleepbg.lock");
+
+    private SleepBGUtil() {
+    }
+
+    public static void enableLock() {
+        try {
+            if (!Files.exists(SLEEP_LOCK_PATH))
+                Files.writeString(SLEEP_LOCK_PATH, "");
+        } catch (IOException ignored) {
+        }
+    }
+
+    public static void disableLock() {
+        try {
+            Files.deleteIfExists(SLEEP_LOCK_PATH);
+        } catch (IOException ignored) {
+        }
+    }
+}
