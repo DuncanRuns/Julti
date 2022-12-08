4aabf08be6a4a0d450f4b05588bd290f93659884
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2022-12-07 21:51:02 -0400

    Update JultiOptions.java

--- src/main/java/xyz/duncanruns/julti/JultiOptions.java
+++ src/main/java/xyz/duncanruns/julti/JultiOptions.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.InstanceCreator;
 import org.apache.commons.io.FilenameUtils;
-import xyz.duncanruns.julti.util.FileUtil;
 import xyz.duncanruns.julti.util.HotkeyUtil;
 import xyz.duncanruns.julti.util.MonitorUtil;
 import xyz.duncanruns.julti.win32.Win32Con;
