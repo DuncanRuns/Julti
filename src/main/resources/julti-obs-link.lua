9026c8278316274ce6853510d1fc71235c54f4ff
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2023-01-07 00:25:39 -0400

    Update julti-obs-link.lua

--- src/main/resources/julti-obs-link.lua
+++ src/main/resources/julti-obs-link.lua
@@ -1,6 +1,6 @@
 --[[
 
-    Julti OBS Link v0.0.1
+    Julti OBS Link v1.0.0
     
     The purpose of the OBS Link Script is to generate and control a Julti scene to assist with multi-instance speedrunning.
 
