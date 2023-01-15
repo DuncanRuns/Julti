5b6f5fa334557682b2ec1a5a53f05b9709b7ad22
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2023-01-15 11:37:01 -0400

    Only do clean wall when using wall, also refactor

--- src/main/java/xyz/duncanruns/julti/instance/MinecraftInstance.java
+++ src/main/java/xyz/duncanruns/julti/instance/MinecraftInstance.java
@@ -466,7 +466,7 @@ public class MinecraftInstance {
                     if (options.coopMode) {
                         openToLan(!options.unpauseOnSwitch);
                     }
-                    if (isUsingF1() && options.unpauseOnSwitch && options.cleanWall) {
+                    if (shouldDoCleanWall()) {
                         pressF1();
                     }
                     if (options.autoFullscreen) {
@@ -479,6 +479,11 @@ public class MinecraftInstance {
         } else {
             log(Level.WARN, "Could not activate instance " + getName() + " (not opened)");
         }
+    }
+
+    private boolean shouldDoCleanWall() {
+        JultiOptions options = JultiOptions.getInstance();
+        return options.resetMode != 0 && isUsingF1() && options.unpauseOnSwitch && options.cleanWall;
     }
 
     public void ensureWindowState() {
@@ -917,7 +922,7 @@ public class MinecraftInstance {
     private void finishWorldLoad(Julti julti) {
         JultiOptions options = JultiOptions.getInstance();
         boolean active = isActive();
-        if (isUsingF1() && options.unpauseOnSwitch && options.cleanWall) {
+        if (shouldDoCleanWall()) {
             // Simple xor considers all 4 cases of f1:true vs f1:false combined with instance currently active
             if (active ^ f1SS == 0) {
                 pressF1();
