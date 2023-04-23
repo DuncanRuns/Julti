175608476b06f6ed387814e166a99c1474f2c133
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2023-04-23 16:30:51 -0300

    Add general error catching & shutdown with popup

--- src/main/java/xyz/duncanruns/julti/Main.java
+++ src/main/java/xyz/duncanruns/julti/Main.java
@@ -5,12 +5,27 @@ import xyz.duncanruns.julti.affinity.AffinityManager;
 import xyz.duncanruns.julti.gui.JultiGUI;
 import xyz.duncanruns.julti.hotkey.HotkeyManager;
 import xyz.duncanruns.julti.script.ScriptManager;
+import xyz.duncanruns.julti.util.KeyboardUtil;
+
+import javax.swing.*;
 
 public final class Main {
     private Main() {
     }
 
     public static void main(String[] args) {
+        try {
+            runJultiApp();
+        } catch (Exception e) {
+            int ans = JOptionPane.showOptionDialog(null, "Julti has crashed during startup or main loop!", "Julti: Crash", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE, null, new Object[]{"Copy Error", "Cancel"}, "Copy Error");
+            if (ans == 0) {
+                KeyboardUtil.copyToClipboard("Error during startup or main loop: " + e);
+            }
+            System.exit(1);
+        }
+    }
+
+    private static void runJultiApp() {
         // Setup GUI theme
         FlatDarkLaf.setup();
 
