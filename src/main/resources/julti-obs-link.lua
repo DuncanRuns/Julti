20cb39c18f4125bfe30519c18cf28dfd9e31f5c7
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2023-06-02 16:03:24 -0300

    Update julti-obs-link.lua

--- src/main/resources/julti-obs-link.lua
+++ src/main/resources/julti-obs-link.lua
@@ -52,6 +52,7 @@ center_align_instances = false
 
 -- Constants
 ALIGN_TOP_LEFT = 5 -- equivalent to obs.OBS_ALIGN_TOP | obs.OBS_ALIGN_LEFT
+ALIGN_CENTER = 0   -- equivalent to obs.OBS_ALIGN_CENTER
 
 ---- File Functions ----
 
@@ -231,7 +232,7 @@ function set_position_with_bounds(scene_item, x, y, width, height, center_align)
     end
 
     -- set alignment of the scene item to: center_align ? CENTER : TOP_LEFT
-    obs.obs_sceneitem_set_alignment(scene_item, center_align and 0 or ALIGN_TOP_LEFT)
+    obs.obs_sceneitem_set_alignment(scene_item, center_align and ALIGN_CENTER or ALIGN_TOP_LEFT)
 
     set_position(scene_item, x + (center_align and total_width / 2 or 0), y + (center_align and total_height / 2 or 0))
 end
