e7b19f8ab5f986bb9a981e686d01d6e21cdbc3e0
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2023-06-20 01:23:25 -0300

    Rearrange McKeyUtil and add LWJGL stuff

--- src/main/java/xyz/duncanruns/julti/util/McKeyUtil.java
+++ src/main/java/xyz/duncanruns/julti/util/McKeyUtil.java
@@ -3,12 +3,27 @@ package xyz.duncanruns.julti.util;
 import com.sun.jna.platform.win32.Win32VK;
 
 import java.util.Hashtable;
+import java.util.Map;
 
 public final class McKeyUtil {
     public static final Hashtable<String, Integer> TRANSLATIONS_TO_GLFW = mapTranslationsToGLFW();
     public static final Hashtable<Integer, Integer> GLFW_TO_VK = mapGLFWToVK();
+    public static final Hashtable<Integer, Integer> LWJGL_TO_VK = mapLWJGLToVK();
 
     private McKeyUtil() {
+    }
+
+    private static Hashtable<Integer, Integer> mapLWJGLToVK() {
+        Hashtable<Integer, Integer> table = new Hashtable<>();
+        for (Map.Entry<Integer, String> entry : mapLWJGLToTranslations().entrySet()) {
+            Integer a = entry.getKey();
+            Integer b = McKeyUtil.getVkFromMCTranslation(entry.getValue());
+            if (b == null) {
+                continue;
+            }
+            table.put(a, b);
+        }
+        return table;
     }
 
     private static Hashtable<String, Integer> mapTranslationsToGLFW() {
@@ -156,26 +171,128 @@ public final class McKeyUtil {
         return table;
     }
 
-    public static Integer getVkFromMCTranslation(String translationKey) {
-        if (translationKey == null) {
-            return null;
+    private static Hashtable<Integer, String> mapLWJGLToTranslations() {
+        Hashtable<Integer, String> table = new Hashtable<>();
+        for (int i = -100; i < 0; i++) {
+            int j = i + 100;
+            String string = j == 0 ? "key.mouse.left" : (j == 1 ? "key.mouse.right" : (j == 2 ? "key.mouse.middle" : "key.mouse." + (j + 1)));
+            table.put(i, string);
         }
-        Integer glfwKey = TRANSLATIONS_TO_GLFW.get(translationKey);
-        Integer vkKey = getVkFromGLFW(glfwKey);
-        if (vkKey == null || vkKey < 0) {
-            return null;
-        }
-        return vkKey;
+        table.put(11, "key.keyboard.0");
+        table.put(2, "key.keyboard.1");
+        table.put(3, "key.keyboard.2");
+        table.put(4, "key.keyboard.3");
+        table.put(5, "key.keyboard.4");
+        table.put(6, "key.keyboard.5");
+        table.put(7, "key.keyboard.6");
+        table.put(8, "key.keyboard.7");
+        table.put(9, "key.keyboard.8");
+        table.put(10, "key.keyboard.9");
+        table.put(30, "key.keyboard.a");
+        table.put(40, "key.keyboard.apostrophe");
+        table.put(48, "key.keyboard.b");
+        table.put(43, "key.keyboard.backslash");
+        table.put(14, "key.keyboard.backspace");
+        table.put(46, "key.keyboard.c");
+        table.put(58, "key.keyboard.caps.lock");
+        table.put(51, "key.keyboard.comma");
+        table.put(32, "key.keyboard.d");
+        table.put(211, "key.keyboard.delete");
+        table.put(208, "key.keyboard.down");
+        table.put(18, "key.keyboard.e");
+        table.put(207, "key.keyboard.end");
+        table.put(28, "key.keyboard.enter");
+        table.put(13, "key.keyboard.equal");
+        table.put(1, "key.keyboard.escape");
+        table.put(33, "key.keyboard.f");
+        table.put(59, "key.keyboard.f1");
+        table.put(68, "key.keyboard.f10");
+        table.put(87, "key.keyboard.f11");
+        table.put(88, "key.keyboard.f12");
+        table.put(100, "key.keyboard.f13");
+        table.put(101, "key.keyboard.f14");
+        table.put(102, "key.keyboard.f15");
+        table.put(103, "key.keyboard.f16");
+        table.put(104, "key.keyboard.f17");
+        table.put(105, "key.keyboard.f18");
+        table.put(113, "key.keyboard.f19");
+        table.put(60, "key.keyboard.f2");
+        table.put(61, "key.keyboard.f3");
+        table.put(62, "key.keyboard.f4");
+        table.put(63, "key.keyboard.f5");
+        table.put(64, "key.keyboard.f6");
+        table.put(65, "key.keyboard.f7");
+        table.put(66, "key.keyboard.f8");
+        table.put(67, "key.keyboard.f9");
+        table.put(34, "key.keyboard.g");
+        table.put(41, "key.keyboard.grave.accent");
+        table.put(35, "key.keyboard.h");
+        table.put(199, "key.keyboard.home");
+        table.put(23, "key.keyboard.i");
+        table.put(210, "key.keyboard.insert");
+        table.put(36, "key.keyboard.j");
+        table.put(37, "key.keyboard.k");
+        table.put(82, "key.keyboard.keypad.0");
+        table.put(79, "key.keyboard.keypad.1");
+        table.put(80, "key.keyboard.keypad.2");
+        table.put(81, "key.keyboard.keypad.3");
+        table.put(75, "key.keyboard.keypad.4");
+        table.put(76, "key.keyboard.keypad.5");
+        table.put(77, "key.keyboard.keypad.6");
+        table.put(71, "key.keyboard.keypad.7");
+        table.put(72, "key.keyboard.keypad.8");
+        table.put(73, "key.keyboard.keypad.9");
+        table.put(78, "key.keyboard.keypad.add");
+        table.put(83, "key.keyboard.keypad.decimal");
+        table.put(181, "key.keyboard.keypad.divide");
+        table.put(156, "key.keyboard.keypad.enter");
+        table.put(141, "key.keyboard.keypad.equal");
+        table.put(55, "key.keyboard.keypad.multiply");
+        table.put(74, "key.keyboard.keypad.subtract");
+        table.put(38, "key.keyboard.l");
+        table.put(203, "key.keyboard.left");
+        table.put(56, "key.keyboard.left.alt");
+        table.put(26, "key.keyboard.left.bracket");
+        table.put(29, "key.keyboard.left.control");
+        table.put(42, "key.keyboard.left.shift");
+        table.put(219, "key.keyboard.left.win");
+        table.put(50, "key.keyboard.m");
+        table.put(12, "key.keyboard.minus");
+        table.put(49, "key.keyboard.n");
+        table.put(69, "key.keyboard.num.lock");
+        table.put(24, "key.keyboard.o");
+        table.put(25, "key.keyboard.p");
+        table.put(209, "key.keyboard.page.down");
+        table.put(201, "key.keyboard.page.up");
+        table.put(197, "key.keyboard.pause");
+        table.put(52, "key.keyboard.period");
+        table.put(183, "key.keyboard.print.screen");
+        table.put(16, "key.keyboard.q");
+        table.put(19, "key.keyboard.r");
+        table.put(205, "key.keyboard.right");
+        table.put(184, "key.keyboard.right.alt");
+        table.put(27, "key.keyboard.right.bracket");
+        table.put(157, "key.keyboard.right.control");
+        table.put(54, "key.keyboard.right.shift");
+        table.put(220, "key.keyboard.right.win");
+        table.put(31, "key.keyboard.s");
+        table.put(70, "key.keyboard.scroll.lock");
+        table.put(39, "key.keyboard.semicolon");
+        table.put(53, "key.keyboard.slash");
+        table.put(57, "key.keyboard.space");
+        table.put(20, "key.keyboard.t");
+        table.put(15, "key.keyboard.tab");
+        table.put(22, "key.keyboard.u");
+        table.put(200, "key.keyboard.up");
+        table.put(47, "key.keyboard.v");
+        table.put(17, "key.keyboard.w");
+        table.put(45, "key.keyboard.x");
+        table.put(21, "key.keyboard.y");
+        table.put(44, "key.keyboard.z");
+        return table;
     }
 
-    public static Integer getVkFromGLFW(Integer key) {
-        if (key == null) {
-            return null;
-        }
-        return GLFW_TO_VK.getOrDefault(key, -1);
-    }
-
-    public static Integer getVkFromGLFWInternal(Integer key) {
+    private static Integer getVkFromGLFWInternal(Integer key) {
         if (key <= 7 && key >= -1)  // Unknown or Mouse
         {
             return -1;
@@ -389,5 +506,31 @@ public final class McKeyUtil {
             return -1; // Definitely wrong
         }
         return -1;
+    }
+
+    public static Integer getVkFromMCTranslation(String translationKey) {
+        if (translationKey == null) {
+            return null;
+        }
+        Integer glfwKey = TRANSLATIONS_TO_GLFW.get(translationKey);
+        Integer vkKey = getVkFromGLFW(glfwKey);
+        if (vkKey == null || vkKey < 0) {
+            return null;
+        }
+        return vkKey;
+    }
+
+    public static Integer getVkFromGLFW(Integer key) {
+        if (key == null) {
+            return null;
+        }
+        return GLFW_TO_VK.getOrDefault(key, -1);
+    }
+
+    public static Integer getVkFromLWJGL(Integer key) {
+        if (key == null) {
+            return null;
+        }
+        return LWJGL_TO_VK.getOrDefault(key, -1);
     }
 }
