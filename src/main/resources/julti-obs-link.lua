2daa581fc0e3b883870314502238b3f39a8d6095
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2023-09-14 23:15:36 -0300

    Update julti-obs-link.lua

--- src/main/resources/julti-obs-link.lua
+++ src/main/resources/julti-obs-link.lua
@@ -101,7 +101,7 @@ function teleport_off_canvas(num)
     set_position(item, total_width + 1000, 0)
 end
 
-function set_instance_data(num, lock_visible, dirt_cover, x, y, width, height, center_align)
+function set_instance_data(num, lock_visible, dirt_cover, freeze_active, x, y, width, height, center_align)
     center_align = center_align or false
 
     local group = get_group_as_scene("Instance " .. num)
@@ -122,9 +122,21 @@ function set_instance_data(num, lock_visible, dirt_cover, x, y, width, height, c
     obs.obs_sceneitem_set_visible(item, dirt_cover)
     set_position_with_bounds(item, 0, 0, width, height, center_align)
 
-    -- Minecraft capture: position and bounds
+    -- Minecraft capture: position, bounds
     local item = obs.obs_scene_find_source(group, "Minecraft Capture " .. num)
     set_position_with_bounds(item, 0, 0, width, height, center_align)
+
+    -- Freeze filter activation for minecraft capture
+    local source = obs.obs_sceneitem_get_source(item)
+    local filter = obs.obs_source_get_filter_by_name(source, "Freeze filter")
+    if filter == nil then
+        local settings = obs.obs_data_create()
+        filter = obs.obs_source_create_private("freeze_filter", "Freeze filter", settings)
+        obs.obs_source_filter_add(source, filter)
+    end
+    if not (filter == nil) then
+        obs.obs_source_set_enabled(filter, freeze_active)
+    end
 
     -- Instance Group: position
     local scene = get_scene("Julti")
@@ -133,17 +145,37 @@ function set_instance_data(num, lock_visible, dirt_cover, x, y, width, height, c
 end
 
 function set_instance_data_from_string(instance_num, data_string)
-    -- data_string format: lock/cover state (1 = lock, 2 = cover, 3 = both, 0 = neither), x, y, w, h
+    -- data_string format: lock/cover state (flag bits: locked, dirt cover, freeze filter), x, y, w, h
     -- Example: "2,0,0,960,540"
     local nums = split_string(data_string, ",")
+
+    local flagBits = tonumber(nums[1])
+
+    local freezeActive = flagBits >= 4
+    if freezeActive then
+        flagBits = flagBits - 4
+    end
+
+    local coverVisible = flagBits >= 2
+    if coverVisible then
+        flagBits = flagBits - 2
+    end
+
+    local lockVisible = flagBits >= 1
+    if lockVisible then
+        flagBits = flagBits - 1
+    end
+
     set_instance_data(
-        instance_num,                         --instance number
-        (nums[1] == "1") or (nums[1] == "3"), -- lock visible
-        (nums[1] == "2") or (nums[1] == "3"), -- cover visible
-        tonumber(nums[2]),                    -- x
-        tonumber(nums[3]),                    -- y
-        tonumber(nums[4]),                    -- width
-        tonumber(nums[5]))                    -- height
+        instance_num,      -- instance number
+        lockVisible,       -- lock visible
+        coverVisible,      -- cover visible
+        freezeActive,      -- freeze filter active
+        tonumber(nums[2]), -- x
+        tonumber(nums[3]), -- y
+        tonumber(nums[4]), -- width
+        tonumber(nums[5])  -- height
+    )
 end
 
 ---- Misc Functions ----
@@ -733,11 +765,13 @@ function make_minecraft_group(num, total_width, total_height, y, i_height)
         source = obs.obs_source_create("game_capture", "Minecraft Capture " .. num, settings, nil)
     end
 
+    S.obs_source_filter_remove(source, S.obs_source_get_filter_by_name(source, "Freeze filter"))
+
     obs.obs_data_release(settings)
     local mcsi = obs.obs_scene_add(scene, source)
     obs.obs_sceneitem_group_add_item(group_si, mcsi)
     set_position_with_bounds(mcsi, 0, 0, total_width, total_height)
-    set_instance_data(num, false, false, 0, y, total_width, i_height)
+    set_instance_data(num, false, false, false, 0, y, total_width, i_height)
     release_source(source)
 end
 
@@ -869,7 +903,7 @@ function loop()
     if user_location ~= "W" then
         local scene = get_scene("Julti")
         bring_to_top(obs.obs_scene_find_source(scene, "Instance " .. user_location))
-        set_instance_data(tonumber(user_location), false, false, 0, 0, total_width, total_height, center_align_instances)
+        set_instance_data(tonumber(user_location), false, false, false, 0, 0, total_width, total_height, center_align_instances)
 
         -- hide bordering instances
         if not center_align_instances then
