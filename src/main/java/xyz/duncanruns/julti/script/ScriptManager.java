b2bb384d9e1acb06704402abc25db3bfd1fc6855
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2023-12-16 04:23:55 -0400

    Move default scripts to a resource file

--- src/main/java/xyz/duncanruns/julti/script/ScriptManager.java
+++ src/main/java/xyz/duncanruns/julti/script/ScriptManager.java
@@ -1,5 +1,6 @@
 package xyz.duncanruns.julti.script;
 
+import com.google.common.io.Resources;
 import org.apache.logging.log4j.Level;
 import xyz.duncanruns.julti.Julti;
 import xyz.duncanruns.julti.JultiOptions;
@@ -10,6 +11,7 @@ import xyz.duncanruns.julti.util.ExceptionUtil;
 import xyz.duncanruns.julti.util.FileUtil;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
@@ -21,14 +23,21 @@ import static xyz.duncanruns.julti.Julti.log;
 
 public class ScriptManager {
     private static final Path SCRIPTS_PATH = JultiOptions.getJultiDir().resolve("scripts.txt");
-    private static final String DEFAULT_SCRIPTS = "Warmup + Launch;0;log Launching all instances...; launch all; wait launch all; log Waiting 5 seconds for title screen...; sleep 5000; log Activating all instances to remove mouse jank...; activate all; activate wall; log Waiting 8 seconds for obs pickup...; sleep 8000; log Resetting all and waiting for world load...; reset all; wait load all; log Unpausing all instances...; activate all; activate wall; log Waiting 20 seconds for warmup...; sleep 20000; log Resetting all and returning to wall...; reset all; activate wall; log Warmup done;\n" +
-            "Start Coping;1;opentolan; chatmessage /gamemode spectator;\n" +
-            "Fight Dragon;1;opentolan; chatmessage /gamemode creative; chatmessage /clear; chatmessage /effect give @s minecraft:saturation 10 100; chatmessage /replaceitem entity @s weapon.offhand bread 3; chatmessage /replaceitem entity @s hotbar.8 cobblestone 23; chatmessage /replaceitem entity @s hotbar.7 ender_pearl 5; chatmessage /give @s iron_axe; chatmessage /give @s iron_pickaxe; chatmessage /give @s iron_shovel; chatmessage /give @s water_bucket; chatmessage /give @s flint_and_steel; chatmessage /give @s bread 3; chatmessage /give @s string 60; chatmessage /give @s oak_planks 17; chatmessage /give @s obsidian 4; chatmessage /give @s crafting_table; chatmessage /gamemode survival; chatmessage /give @s oak_boat; chatmessage /setblock ~ ~ ~ end_portal;";
     private static final List<Script> SCRIPTS = new CopyOnWriteArrayList<>();
     private static CancelRequester cancelRequester = CancelRequesters.ALWAYS_CANCEL_REQUESTER; // Will change from fake requester to other requesters
     private static Thread runningScriptThread = null;
-
     private static final Object LOCK = new Object();
+
+
+    private static String getDefaultScripts() {
+        String defaultScripts;
+        try {
+            defaultScripts = Resources.toString(Resources.getResource("defaultscripts.txt"), Charset.defaultCharset());
+        } catch (IOException e) {
+            defaultScripts = "";
+        }
+        return defaultScripts;
+    }
 
     public static void reload() {
         String scriptsFileContents = "";
@@ -36,10 +45,10 @@ public class ScriptManager {
             try {
                 scriptsFileContents = FileUtil.readString(SCRIPTS_PATH);
             } catch (IOException e) {
-                scriptsFileContents = DEFAULT_SCRIPTS;
+                scriptsFileContents = getDefaultScripts();
             }
         } else {
-            scriptsFileContents = DEFAULT_SCRIPTS;
+            scriptsFileContents = getDefaultScripts();
         }
 
         SCRIPTS.clear();
