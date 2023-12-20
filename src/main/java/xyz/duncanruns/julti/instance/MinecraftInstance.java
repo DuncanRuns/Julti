fe9082f1620448c353488acb32075ac7212ab695
itsdxrk <tanzeemr2@gmail.com>
2023-12-20 19:53:04 +0000

    Check for SpeedrunIGT v14 (for paceman)

--- src/main/java/xyz/duncanruns/julti/instance/MinecraftInstance.java
+++ src/main/java/xyz/duncanruns/julti/instance/MinecraftInstance.java
@@ -180,6 +180,17 @@ public class MinecraftInstance {
             Julti.log(Level.WARN, "Warning: Instance \"" + this + " does not have the standard settings mod!");
         }
 
+        FabricJarInfo igtInfo = FabricJarUtil.getJarInfo(this.gameOptions.jars, "speedrunigt");
+
+        boolean trackableIGT = true;
+        Matcher igtMatcher = Pattern.compile("(?:1[4-9]|[2-9]\\d|\\d{3,})\\.\\d+\\+1\\.16\\.1").matcher(igtInfo.version);
+        if (!igtMatcher.find()) {
+            trackableIGT = false;
+        }
+
+        if (!trackableIGT) {
+            Julti.log(Level.WARN, "Warning: Instance \"" + this + "\" does not have at least version 14 of SpeedrunIGT! If using PaceMan, runs will not be sent.");
+        }
     }
 
     private void discoverName() {
