bb52f1af5b18115c44d661f54d3f5ecd59d6f222
itsdxrk <tanzeemr2@gmail.com>
2024-01-16 19:38:21 +0000

    Check that version used is for 1.16.1

--- src/main/java/xyz/duncanruns/julti/instance/MinecraftInstance.java
+++ src/main/java/xyz/duncanruns/julti/instance/MinecraftInstance.java
@@ -183,8 +183,9 @@ public class MinecraftInstance {
         FabricJarInfo igtInfo = FabricJarUtil.getJarInfo(this.gameOptions.jars, "speedrunigt");
 
         boolean trackableIGT = VersionUtil.tryCompare(igtInfo.version.split("\\+")[0], "14.0", -1) >= 0;
+        String igtMCVersion = igtInfo.version.split("\\+")[0];
 
-        if (!trackableIGT) {
+        if (!trackableIGT && igtMCVersion.equals("1.16.1")) {
             Julti.log(Level.WARN, "Warning: Instance \"" + this + "\" does not have at least version 14 of SpeedrunIGT! If using PaceMan, runs will not be sent.");
         }
     }
