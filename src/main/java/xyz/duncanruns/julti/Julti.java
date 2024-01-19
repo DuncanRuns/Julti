d68f76e145d288644d6231a2bc697041f60656dd
draconix6 <30545768+draconix6@users.noreply.github.com>
2024-01-20 10:26:10 +1300

    undo minimize projector change

--- src/main/java/xyz/duncanruns/julti/Julti.java
+++ src/main/java/xyz/duncanruns/julti/Julti.java
@@ -311,10 +311,10 @@ public final class Julti {
 
     public void activateInstance(MinecraftInstance instance, boolean doingSetup) {
         JultiOptions options = JultiOptions.getJultiOptions();
+        instance.activate(doingSetup);
         if ((options.alwaysOnTopProjector || options.minimizeProjectorWhenPlaying) && ActiveWindowManager.isWallActive()) {
             User32.INSTANCE.ShowWindow(ActiveWindowManager.getActiveHwnd(), User32.SW_MINIMIZE);
         }
-        instance.activate(doingSetup);
         OBSStateManager.getOBSStateManager().setLocation(InstanceManager.getInstanceManager().getInstanceNum(instance));
     }
 
