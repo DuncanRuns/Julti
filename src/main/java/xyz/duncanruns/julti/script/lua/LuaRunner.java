51356cd850db686e32c9aa78280d6b9dca78102c
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-04-16 18:17:24 -0300

    Remove dangerous globals from lua runner

--- src/main/java/xyz/duncanruns/julti/script/lua/LuaRunner.java
+++ src/main/java/xyz/duncanruns/julti/script/lua/LuaRunner.java
@@ -1,12 +1,11 @@
 package xyz.duncanruns.julti.script.lua;
 
 import org.apache.logging.log4j.Level;
-import org.luaj.vm2.Globals;
-import org.luaj.vm2.LuaError;
-import org.luaj.vm2.LuaValue;
-import org.luaj.vm2.Varargs;
-import org.luaj.vm2.lib.DebugLib;
-import org.luaj.vm2.lib.jse.JsePlatform;
+import org.luaj.vm2.*;
+import org.luaj.vm2.compiler.LuaC;
+import org.luaj.vm2.lib.*;
+import org.luaj.vm2.lib.jse.JseBaseLib;
+import org.luaj.vm2.lib.jse.JseMathLib;
 import xyz.duncanruns.julti.Julti;
 import xyz.duncanruns.julti.cancelrequester.CancelRequester;
 import xyz.duncanruns.julti.util.ExceptionUtil;
@@ -20,7 +19,7 @@ public class LuaRunner {
     public static final Map<String, LuaValue> GLOBALS_MAP = Collections.synchronizedMap(new HashMap<>());
 
     public static void runLuaScript(String luaScript, CancelRequester cancelRequester) {
-        Globals globals = JsePlatform.standardGlobals();
+        Globals globals = getSafeGlobals();
         globals.load(new InterruptibleDebugLib(cancelRequester));
         LuaLibraries.addLibraries(globals, cancelRequester);
         LuaValue chunk = globals.load(luaScript);
@@ -31,6 +30,19 @@ public class LuaRunner {
                 Julti.log(Level.ERROR, "Error while executing script: " + ExceptionUtil.toDetailedString(e.getCause() != null ? e.getCause() : e));
             }
         }
+    }
+
+    private static Globals getSafeGlobals() {
+        Globals globals = new Globals();
+        globals.load(new JseBaseLib());
+        globals.load(new PackageLib());
+        globals.load(new Bit32Lib());
+        globals.load(new TableLib());
+        globals.load(new StringLib());
+        globals.load(new JseMathLib());
+        LoadState.install(globals);
+        LuaC.install(globals);
+        return globals;
     }
 
     private static class LuaScriptCancelledException extends RuntimeException {
