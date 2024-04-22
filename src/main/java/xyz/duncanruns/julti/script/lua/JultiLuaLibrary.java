a838b276dcbce63fcb1c57a0640822ac33550f22
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-04-21 23:59:16 -0300

    Add docs and fix some lua functions

--- src/main/java/xyz/duncanruns/julti/script/lua/JultiLuaLibrary.java
+++ src/main/java/xyz/duncanruns/julti/script/lua/JultiLuaLibrary.java
@@ -56,10 +56,12 @@ class JultiLuaLibrary extends LuaLibrary {
         return this.script.getName();
     }
 
+    @LuaDocumentation(description = "Brings the user to the instance specified by instanceNum. Optionally a second argument (doSetupStyle) can be specified to prevent window resizing, fullscreening, unpausing, etc.")
     public void activateInstance(int instanceNum, Boolean doSetupStyle) {
         Julti.waitForExecute(() -> Julti.getJulti().activateInstance(getInstanceFromInt(instanceNum), !(doSetupStyle == null) && doSetupStyle));
     }
 
+    @LuaDocumentation(description = "Sends a chat message in the active instance. A slash needs to be given if executing a command (eg. julti.sendChatMessage(\"/kill\")).")
     public void sendChatMessage(String message) {
         Julti.waitForExecute(() -> {
             MinecraftInstance selectedInstance = InstanceManager.getInstanceManager().getSelectedInstance();
@@ -70,49 +72,60 @@ class JultiLuaLibrary extends LuaLibrary {
         });
     }
 
+    @LuaDocumentation(description = "Clears all but the last 5 worlds in each instance.")
     public void clearWorlds() {
         Julti.waitForExecute(BopperUtil::clearWorlds);
     }
 
+    @LuaDocumentation(description = "Closes a specific instance.")
     public void closeInstance(int instanceNum) {
         Julti.waitForExecute(() -> getInstanceFromInt(instanceNum).reset());
     }
 
+    @LuaDocumentation(description = "Closes all instances.")
     public void closeAllInstances() {
         Julti.waitForExecute(() -> DoAllFastUtil.doAllFast(MinecraftInstance::closeWindow));
     }
 
+    @LuaDocumentation(description = "Replicates a hotkey action exactly. Possible codes are: \"reset\", \"bgReset\", \"wallReset\", \"wallSingleReset\", \"wallLock\", \"wallPlay\", \"wallFocusReset\", \"wallPlayLock\", \"debugHover\", or \"script:<script name>\"\nA mouse position can also be specified with 2 extra arguments, or if left blank will use the user's mouse position.")
     public void replicateHotkey(String hotkeyCode, Integer mouseX, Integer mouseY) {
         Point mousePos = mouseX == null ? MouseUtil.getMousePos() : new Point(mouseX, mouseY);
         Julti.getJulti().queueMessageAndWait(new HotkeyPressQMessage(hotkeyCode, mousePos));
     }
 
+    @LuaDocumentation(description = "Launches a specific instance. Does nothing if the instance is already opened. Requires a multimc executable to be specified in Julti options.")
     public void launchInstance(int instanceNum) {
         Julti.waitForExecute(() -> SafeInstanceLauncher.launchInstance(getInstanceFromInt(instanceNum)));
     }
 
+    @LuaDocumentation(description = "Launches all unopened instances. Requires a multimc executable to be specified in Julti options.")
     public void launchAllInstances() {
         Julti.waitForExecute(() -> SafeInstanceLauncher.launchInstances(InstanceManager.getInstanceManager().getInstances()));
     }
 
+    @LuaDocumentation(description = "Locks a specific instance.")
     public void lockInstance(int instanceNum) {
         Julti.waitForExecute(() -> ResetHelper.getManager().lockInstance(getInstanceFromInt(instanceNum)));
     }
 
+    @LuaDocumentation(description = "Locks all instances.")
     public void lockAllInstances() {
         Julti.waitForExecute(() -> InstanceManager.getInstanceManager().getInstances().forEach(instance -> ResetHelper.getManager().lockInstance(instance)));
     }
 
+    @LuaDocumentation(description = "Logs a message into the Julti log. If a script is ran in customization mode, this won't log anything until julti.isCustomization() is checked.")
     @AllowedWhileCustomizing
     public void log(String message) {
         Julti.log(Level.INFO, message);
     }
 
+    @LuaDocumentation(description = "Opens the specified file as if it were double-clicked in file explorer.")
     @AllowedWhileCustomizing
     public void openFile(String filePath) {
         LauncherUtil.openFile(filePath);
     }
 
+    @LuaDocumentation(description = "Opens the currently active instance's world to lan.")
     public void openInstanceToLan() {
         Julti.waitForExecute(() -> {
             MinecraftInstance selectedInstance = InstanceManager.getInstanceManager().getSelectedInstance();
@@ -122,16 +135,14 @@ class JultiLuaLibrary extends LuaLibrary {
         });
     }
 
+    @LuaDocumentation(description = "Sets the Julti option to the given value after attempting to convert it.")
     @AllowedWhileCustomizing
-
     public boolean trySetOption(String optionName, String optionValue) {
-        AtomicBoolean out = new AtomicBoolean(false);
-        Julti.getJulti().queueMessageAndWait(new OptionChangeQMessage(optionName, optionValue));
-        return out.get();
+        return Julti.getJulti().queueMessageAndWait(new OptionChangeQMessage(optionName, optionValue));
     }
 
+    @LuaDocumentation(description = "Gets a Julti option and returns it as a string.")
     @AllowedWhileCustomizing
-
     public String getOptionAsString(String optionName) {
         AtomicReference<String> out = new AtomicReference<>("");
         Julti.waitForExecute(() -> {
@@ -143,6 +154,7 @@ class JultiLuaLibrary extends LuaLibrary {
         return out.get();
     }
 
+    @LuaDocumentation(description = "Plays a sound file located in the .Julti/sounds folder. A full path to a sound can also be specified.")
     @AllowedWhileCustomizing
     public boolean tryPlaySound(String soundLocation, float volume) {
         Path pathFromSoundsFolder = JultiOptions.getJultiDir().resolve("sounds").resolve(soundLocation);
@@ -158,14 +170,17 @@ class JultiLuaLibrary extends LuaLibrary {
         return false;
     }
 
+    @LuaDocumentation(description = "Resets the specified instance.")
     public void resetInstance(int instanceNum) {
         Julti.waitForExecute(() -> getInstanceFromInt(instanceNum).reset());
     }
 
+    @LuaDocumentation(description = "Resets all instances, regardless of locks.")
     public void resetAllInstances() {
         Julti.waitForExecute(() -> DoAllFastUtil.doAllFast(MinecraftInstance::reset));
     }
 
+    @LuaDocumentation(description = "Sets the session resets to the given integer.")
     public void setSessionResets(int sessionResets) {
         Julti.waitForExecute(() -> {
             ResetCounter.sessionCounter = sessionResets;
@@ -174,10 +189,12 @@ class JultiLuaLibrary extends LuaLibrary {
         });
     }
 
+    @LuaDocumentation(description = "Sleeps for the specified amount of milliseconds.")
     public void sleep(long millis) {
         SleepUtil.sleep(millis);
     }
 
+    @LuaDocumentation(description = "Waits for an instance to launch.")
     public void waitForInstanceLaunch(int instanceNum) {
         MinecraftInstance instance = getInstanceFromInt(instanceNum);
         while ((!this.cancelRequester.isCancelRequested())) {
@@ -190,6 +207,7 @@ class JultiLuaLibrary extends LuaLibrary {
         }
     }
 
+    @LuaDocumentation(description = "Waits for an instance's preview to load.")
     public void waitForInstancePreviewLoad(int instanceNum) {
         MinecraftInstance instance = getInstanceFromInt(instanceNum);
         while ((!this.cancelRequester.isCancelRequested())) {
@@ -202,6 +220,7 @@ class JultiLuaLibrary extends LuaLibrary {
         }
     }
 
+    @LuaDocumentation(description = "Waits for an instance's world to load.")
     public void waitForInstanceLoad(int instanceNum) {
         MinecraftInstance instance = getInstanceFromInt(instanceNum);
         while ((!this.cancelRequester.isCancelRequested())) {
@@ -214,58 +233,70 @@ class JultiLuaLibrary extends LuaLibrary {
         }
     }
 
+    @LuaDocumentation(description = "Presses the escape key on the specified instance.")
     public void pressEscOnInstance(int instanceNum) {
         Julti.waitForExecute(() -> getInstanceFromInt(instanceNum).getKeyPresser().pressEsc());
     }
 
+    @LuaDocumentation(description = "Gets the amount of instances in the instance list.")
     @AllowedWhileCustomizing
     public int getInstanceCount() {
         return InstanceManager.getInstanceManager().getSize();
     }
 
+    @LuaDocumentation(description = "Gets the instance number of the currently selected instance. Returns 0 if no instance is selected.")
     public int getSelectedInstanceNum() {
         InstanceManager instanceManager = InstanceManager.getInstanceManager();
         return instanceManager.getInstanceNum(instanceManager.getSelectedInstance());
     }
 
+    @LuaDocumentation(description = "Runs a Julti command.")
     public void runCommand(String command) {
         CommandManager.getMainManager().runCommand(command, this.cancelRequester);
     }
 
+    @LuaDocumentation(description = "Brings the user to the wall window if it exists.")
     public void focusWall() {
         Julti.waitForExecute(() -> Julti.getJulti().focusWall());
     }
 
+    @LuaDocumentation(description = "Runs a script of the specified name and waits for it to end.")
     public void runScript(String scriptName) {
         ScriptManager.runScriptAndWait(scriptName);
     }
 
+    @LuaDocumentation(description = "Sets a value in global storage. The value can be accessed on other runs of this script or any other script.\nGlobal storage is not persistent through restarts of Julti.\nFor string storage that is private to this specific script and persists through restarts, see julti.getCustomizable and julti.setCustomizable.")
     @AllowedWhileCustomizing
     public void setGlobal(String key, LuaValue val) {
         LuaRunner.GLOBALS_MAP.put(key, val);
     }
 
+    @LuaDocumentation(description = "Retrieves a value from global storage set by julti.setGlobal(). A default value can optionally be provided in the case that no value is found in the globals storage.")
     @AllowedWhileCustomizing
     public LuaValue getGlobal(String key, LuaValue def) {
         return LuaRunner.GLOBALS_MAP.getOrDefault(key, def == null ? NIL : def);
     }
 
+    @LuaDocumentation(description = "Gets the current state of the instance. Returns \"WAITING\", \"INWORLD\", \"TITLE\", \"GENERATING\", or \"PREVIEWING\".")
     public String getInstanceState(int instanceNum) {
         MinecraftInstance instance = getInstanceFromInt(instanceNum);
         return instance.getStateTracker().getInstanceState().name();
     }
 
+    @LuaDocumentation(description = "Gets a more detailed state of the \"INWORLD\" state. Returns \"UNPAUSED\", \"PAUSED\", or \"GAMESCREENOPEN\".")
     public String getInstanceInWorldState(int instanceNum) {
         MinecraftInstance instance = getInstanceFromInt(instanceNum);
         return instance.getStateTracker().getInWorldType().name();
     }
 
+    @LuaDocumentation(description = "Returns true if any Minecraft window is active.")
     public boolean isOnMinecraftWindow() {
         AtomicBoolean out = new AtomicBoolean();
         Julti.waitForExecute(() -> out.set(ActiveWindowManager.isMinecraftActive()));
         return out.get();
     }
 
+    @LuaDocumentation(description = "Simulates holding a key on the keyboard.\nTakes a single character (\"a\", \"b\", \"1\", \"2\") or a VK constant such as \"VK_RETURN\" (https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes)")
     public void keyDown(String key) {
         if (key.startsWith("VK_")) {
             KeyboardUtil.keyDown(Win32VK.valueOf(key));
@@ -278,6 +309,7 @@ class JultiLuaLibrary extends LuaLibrary {
         KeyboardUtil.keyDown(bytes[0]);
     }
 
+    @LuaDocumentation(description = "Simulates releasing a key on the keyboard.\nTakes a single character (\"a\", \"b\", \"1\", \"2\") or a VK constant such as \"VK_RETURN\" (https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes)")
     public void keyUp(String key) {
         if (key.startsWith("VK_")) {
             KeyboardUtil.keyUp(Win32VK.valueOf(key));
@@ -290,49 +322,58 @@ class JultiLuaLibrary extends LuaLibrary {
         KeyboardUtil.keyUp(bytes[0]);
     }
 
+    @LuaDocumentation(description = "Simulates pressing and releasing a key instantly on the keyboard.\nTakes a single character (\"a\", \"b\", \"1\", \"2\") or a VK constant such as \"VK_RETURN\" (https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes)")
     public void pressKey(String key) {
         this.keyDown(key);
         this.keyUp(key);
     }
 
+    @LuaDocumentation(description = "Simulates holding a key on the keyboard and releases it after the specified amount of milliseconds.\nTakes a single character (\"a\", \"b\", \"1\", \"2\") or a VK constant such as \"VK_RETURN\" (https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes)")
     public void holdKey(String key, long millis) {
         this.keyDown(key);
         this.sleep(millis);
         this.keyUp(key);
     }
 
+    @LuaDocumentation(description = "Returns true if an instance from the instances list is active.")
     public boolean isInstanceActive() {
         AtomicBoolean out = new AtomicBoolean();
         Julti.waitForExecute(() -> out.set(InstanceManager.getInstanceManager().getSelectedInstance() != null));
         return out.get();
     }
 
+    @LuaDocumentation(description = "Returns true if the wall window is active.")
     public boolean isWallActive() {
         AtomicBoolean out = new AtomicBoolean();
         Julti.waitForExecute(() -> out.set(ActiveWindowManager.isWallActive()));
         return out.get();
     }
 
+    @LuaDocumentation(description = "Gets the last activation time of the specified instance in milliseconds. Useful in combination with julti.getCurrentTime().")
     public long getLastActivation(int instanceNum) {
         AtomicLong out = new AtomicLong();
         Julti.waitForExecute(() -> out.set(getInstanceFromInt(instanceNum).getLastActivation()));
         return out.get();
     }
 
+    @LuaDocumentation(description = "Gets the current time in milliseconds.")
     @AllowedWhileCustomizing
     public long getCurrentTime() {
         return System.currentTimeMillis();
     }
 
+    @LuaDocumentation(description = "Cancels this running script. A simple return statement can also be used to stop the lua script.")
     @AllowedWhileCustomizing
     public void cancelScript() {
         this.cancelRequester.cancel();
     }
 
+    @LuaDocumentation(description = "Cancels all running scripts.")
     public void cancelAllScripts() {
         ScriptManager.cancelAllScripts();
     }
 
+    @LuaDocumentation(description = "Waits for all instances to launch.")
     public void waitForAllInstancesLaunch() {
         for (MinecraftInstance instance : InstanceManager.getInstanceManager().getInstances()) {
             while ((!this.cancelRequester.isCancelRequested())) {
@@ -346,7 +387,8 @@ class JultiLuaLibrary extends LuaLibrary {
         }
     }
 
-    public void waitForAllInstanceSPreviewLoad() {
+    @LuaDocumentation(description = "Waits for all instances' previews to load.")
+    public void waitForAllInstancesPreviewLoad() {
         for (MinecraftInstance instance : InstanceManager.getInstanceManager().getInstances()) {
             while ((!this.cancelRequester.isCancelRequested())) {
                 AtomicBoolean b = new AtomicBoolean(false);
@@ -359,6 +401,7 @@ class JultiLuaLibrary extends LuaLibrary {
         }
     }
 
+    @LuaDocumentation(description = "Waits for all instances' worlds to load.")
     public void waitForAllInstancesLoad() {
         for (MinecraftInstance instance : InstanceManager.getInstanceManager().getInstances()) {
             while ((!this.cancelRequester.isCancelRequested())) {
@@ -372,11 +415,19 @@ class JultiLuaLibrary extends LuaLibrary {
         }
     }
 
+    @LuaDocumentation(description = "Returns true if the script is being run in customization mode, otherwise false.")
     @AllowedWhileCustomizing
     public boolean isCustomizing() {
         return false;
     }
 
+    @LuaDocumentation(description = "Sets and stores a customizable string. Usually should be done inside of an if julti.isCustomizing() block.\nValues stored are only accessible to runs of this script and are persistent through Julti restarts.")
+    @AllowedWhileCustomizing
+    public void setCustomizable(String key, String value) {
+        CustomizableManager.set(this.script.getName(), key, value);
+    }
+
+    @LuaDocumentation(description = "Gets a stored customizable string. A default value can optionally be provided in the case that no value is found in the customizables storage.")
     @AllowedWhileCustomizing
     @Nullable
     public String getCustomizable(String key, String def) {
@@ -384,17 +435,14 @@ class JultiLuaLibrary extends LuaLibrary {
         return out == null ? def : out;
     }
 
-    @AllowedWhileCustomizing
-    public void setCustomizable(String key, String value) {
-        CustomizableManager.set(this.script.getName(), key, value);
-    }
-
+    @LuaDocumentation(description = "This function only works during customization.\nPresents the user with a text input box and returns the string entered, or nil if they cancel/close the prompt without pressing Ok.")
     @AllowedWhileCustomizing
     @Nullable
     public String askTextBox(String message, String startingVal) {
         throw new CustomizingException("Script Error: julti.askTextBox used while not customizing.");
     }
 
+    @LuaDocumentation(description = "This function only works during customization.\nPresents the user with a message and Yes/No/Cancel buttons. Returns true for yes, false for no, and nil for cancel or if the user closes the window.")
     @AllowedWhileCustomizing
     @Nullable
     public Boolean askYesNo(String message) {
