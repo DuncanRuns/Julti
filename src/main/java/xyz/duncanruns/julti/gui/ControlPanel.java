aba863f69f0d6fb88803f25cf4f6b888d02a2cf0
dfanm <dfanm26@gmail.com>
2024-05-30 22:53:19 -0700

    Update ControlPanel.java
    change reset instance position to also have playing position in utility mode

--- src/main/java/xyz/duncanruns/julti/gui/ControlPanel.java
+++ src/main/java/xyz/duncanruns/julti/gui/ControlPanel.java
@@ -82,15 +82,28 @@ public class ControlPanel extends JPanel {
                 }
             });
 
-            GUIUtil.addMenuItem(menu, "Reset Instance Positions", new AbstractAction() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    Thread.currentThread().setName("julti-gui");
-                    SleepBGUtil.disableLock();
-                    // ensure instance is unfullscreened and unminimized
-                    Julti.doLater(() -> DoAllFastUtil.doAllFast(minecraftInstance -> minecraftInstance.ensureInitialWindowState()));
-                }
-            });
+            if (!JultiOptions.getJultiOptions().utilityMode) {
+                GUIUtil.addMenuItem(menu, "Reset Instance Positions", new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        Thread.currentThread().setName("julti-gui");
+                        SleepBGUtil.disableLock();
+                        // ensure instance is unfullscreened and unminimized
+                        Julti.doLater(() -> DoAllFastUtil.doAllFast(minecraftInstance -> minecraftInstance.ensureInitialWindowState()));
+                    }
+                });
+            }
+            if (JultiOptions.getJultiOptions().utilityMode) {
+                GUIUtil.addMenuItem(menu, "Set Playing Positions", new AbstractAction() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        Thread.currentThread().setName("julti-gui");
+                        SleepBGUtil.disableLock();
+                        // ensure instance is at playing size
+                        Julti.doLater(() -> DoAllFastUtil.doAllFast(minecraftInstance -> minecraftInstance.ensurePlayingWindowState()));
+                    }
+                });
+            }
 
             Point mousePos = this.getMousePosition();
             if (mousePos == null) {
