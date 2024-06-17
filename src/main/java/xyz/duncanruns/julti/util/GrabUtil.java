06ed7a052f612ad6c43fd316093ccf095c8ce11e
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-06-16 23:11:02 -0300

    Update Official Scripts to not use GH API

--- src/main/java/xyz/duncanruns/julti/util/GrabUtil.java
+++ src/main/java/xyz/duncanruns/julti/util/GrabUtil.java
@@ -1,5 +1,6 @@
 package xyz.duncanruns.julti.util;
 
+import com.google.common.net.UrlEscapers;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSyntaxException;
--- src/main/java/xyz/duncanruns/julti/util/OfficialScriptsUtil.java
+++ src/main/java/xyz/duncanruns/julti/util/OfficialScriptsUtil.java
@@ -1,34 +1,25 @@
 package xyz.duncanruns.julti.util;
 
-import org.apache.commons.io.IOUtils;
-import org.kohsuke.github.GHContent;
+import com.google.common.net.UrlEscapers;
+import com.google.gson.JsonElement;
 import xyz.duncanruns.julti.script.ScriptManager;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class OfficialScriptsUtil {
     public static Set<String> retrieveOfficialScriptFileNames() throws IOException {
-        return GitHubUtil.getGitHub()
-                .getRepository("DuncanRuns/Julti-Scripts")
-                .getDirectoryContent("scripts")
-                .stream()
-                .map(GHContent::getName)
+        return GrabUtil.grabJson("https://raw.githubusercontent.com/DuncanRuns/Julti-Scripts/main/scripts.json")
+                .getAsJsonArray("scripts").asList().stream()
+                .map(JsonElement::getAsString)
                 .filter(s -> s.endsWith(".txt") || s.endsWith(".lua"))
                 .collect(Collectors.toSet());
     }
 
     public static void downloadScript(String scriptFileName) throws IOException {
         String scriptName = scriptFileName.substring(0, scriptFileName.length() - 4);
-        ScriptManager.writeScript(scriptName, IOUtils.toString(
-                GitHubUtil.getGitHub()
-                        .getRepository("DuncanRuns/Julti-Scripts")
-                        .getFileContent("scripts/" + scriptFileName)
-                        .read(),
-                StandardCharsets.UTF_8
-        ), scriptFileName.endsWith(".txt"));
+        ScriptManager.writeScript(scriptName, GrabUtil.grab("https://raw.githubusercontent.com/DuncanRuns/Julti-Scripts/main/scripts/" + UrlEscapers.urlPathSegmentEscaper().escape(scriptFileName)), scriptFileName.endsWith(".txt"));
         ScriptManager.reload();
     }
 }
