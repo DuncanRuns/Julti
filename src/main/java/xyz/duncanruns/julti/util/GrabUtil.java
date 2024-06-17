6e5d34ce712ba7bbfce1484291e017d30217f97f
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-06-16 23:01:50 -0300

    Create GrabUtil.java

---
+++ src/main/java/xyz/duncanruns/julti/util/GrabUtil.java
@@ -0,0 +1,26 @@
+package xyz.duncanruns.julti.util;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public final class GrabUtil {
+    private static final Gson GSON = new Gson();
+
+    private GrabUtil() {
+    }
+
+    public static String grab(String origin) throws IOException {
+        return IOUtils.toString(new BufferedInputStream(new URL(origin).openStream()), StandardCharsets.UTF_8);
+    }
+
+    public static JsonObject grabJson(String origin) throws IOException, JsonSyntaxException {
+        return GSON.fromJson(grab(origin), JsonObject.class);
+    }
+}
