ce4ddcd5a39aaecf66e1a15174c6af4430eaa980
DuncanRuns <59705125+DuncanRuns@users.noreply.github.com>
2024-07-14 04:58:56 -0300

    Add back the truncate message (whoops)

--- src/main/java/xyz/duncanruns/julti/gui/LogPanel.java
+++ src/main/java/xyz/duncanruns/julti/gui/LogPanel.java
@@ -11,10 +11,13 @@ import javax.swing.text.DefaultCaret;
 import java.awt.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class LogPanel extends JPanel {
     private final static int MAX_LOG_CHARS = 3000; // arbitrary number, can be changed
+    private final static String TRUNCATE_MESSAGE = "Logs have been truncated. To view the full logs, go to Options > Other > View Julti Logs.\n\n";
+    private final static int TRUNCATE_MESSAGE_LEN = TRUNCATE_MESSAGE.length();
 
     public LogPanel() {
         this.setupWindow();
@@ -30,6 +33,7 @@ public class LogPanel extends JPanel {
     private void createTextArea() {
         JTextArea textArea = new JTextArea();
         AtomicInteger totalChars = new AtomicInteger();
+        AtomicBoolean truncateMessageAdded = new AtomicBoolean(false);
         LogReceiver.setLogConsumer(s -> {
             if (totalChars.get() > 0) {
                 s = "\n" + s;
@@ -37,19 +41,25 @@ public class LogPanel extends JPanel {
             textArea.append(s);
             totalChars.addAndGet(s.length());
             if (totalChars.get() > MAX_LOG_CHARS) {
+                // Prepend our truncate message
+                if(!truncateMessageAdded.get()){
+                    truncateMessageAdded.set(true);
+                    textArea.insert(TRUNCATE_MESSAGE,0);
+                }
+
                 // We could just remove totalChars.get() - MAX_LOG_CHARS, but that could cut off some lines, so lets cut it off near at a nearby newline
                 // This means once MAX_LOG_CHARS is reached, we actually stay a tiny bit over it
                 int toRemove;
                 try {
-                    toRemove = textArea.getText(0, totalChars.get() - MAX_LOG_CHARS).lastIndexOf("\n");
+                    toRemove = textArea.getText(TRUNCATE_MESSAGE_LEN, TRUNCATE_MESSAGE_LEN + totalChars.get() - MAX_LOG_CHARS).lastIndexOf("\n");
                 } catch (BadLocationException e) {
                     throw new RuntimeException(e);
                 }
                 if (toRemove == -1) {
                     return;
                 }
-                toRemove++; // Include the newline itself for removal, as replaceRange's end is exclusive
-                textArea.replaceRange(null, 0, toRemove);
+                toRemove += 1; // Include the newline itself for removal, and then remove logOffset
+                textArea.replaceRange(null, TRUNCATE_MESSAGE_LEN, toRemove + TRUNCATE_MESSAGE_LEN);
                 totalChars.addAndGet(-toRemove);
             }
         });
